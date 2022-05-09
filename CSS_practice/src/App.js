a3b3447ac9b42e8eb1f1839cc421a681eddbeded
Alex Geis <siegxela@gmail.com>
2022-05-09 16:22:50 -0600

    update export default function app.js

--- CSS_practice/src/App.js
+++ CSS_practice/src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header";
 
-function App() {
+export default function App() {
   return (
     <>
       <Header />
@@ -18,5 +18,3 @@ function App() {
     </>
   );
 }
-
-export default App;
