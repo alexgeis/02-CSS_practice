356740c2a76543c40e2eee57a8f59149134a2399
Alex Geis <siegxela@gmail.com>
2022-05-10 21:05:18 -0600

    abstract out cards

--- CSS_practice/src/App.js
+++ CSS_practice/src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import Card from "./Card";
 //Planned sections
 // -media query
 // -flexbox
@@ -33,42 +34,12 @@ export default function App() {
             </ul>
           </div>
           <section className="content">
-            <div className="card">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora,
-              eveniet? Dolorem dignissimos maiores non delectus possimus dolor
-              nulla repudiandae vitae provident quae, obcaecati ipsam unde
-              impedit corrupti veritatis minima porro?
-            </div>
-            <div className="card">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi
-              quaerat qui iure ipsam maiores velit tempora, deleniti nesciunt
-              fuga suscipit alias vero rem, corporis officia totam saepe
-              excepturi odit ea.
-            </div>
-            <div className="card">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis
-              illo ex quas, commodi eligendi aliquam ut, dolor, atque aliquid
-              iure nulla. Laudantium optio accusantium quaerat fugiat, natus
-              officia esse autem?
-            </div>
-            <div className="card">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Necessitatibus nihil impedit eius amet adipisci dolorum vel
-              nostrum sit excepturi corporis tenetur cum, dolore incidunt
-              blanditiis. Unde earum minima laboriosam eos!
-            </div>
-            <div className="card">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis
-              illo ex quas, commodi eligendi aliquam ut, dolor, atque aliquid
-              iure nulla. Laudantium optio accusantium quaerat fugiat, natus
-              officia esse autem?
-            </div>
-            <div className="card">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Necessitatibus nihil impedit eius amet adipisci dolorum vel
-              nostrum sit excepturi corporis tenetur cum, dolore incidunt
-              blanditiis. Unde earum minima laboriosam eos!
-            </div>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
           </section>
         </div>
         <Footer />
---
+++ CSS_practice/src/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Card(props) {
+  return (
+    <>
+      <div className="card">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis illo ex
+        quas, commodi eligendi aliquam ut, dolor, atque aliquid iure nulla.
+        Laudantium optio accusantium quaerat fugiat, natus officia esse autem?
+      </div>
+      ;
+    </>
+  );
+}
